383e583ef61e3d42be4a79e56381c8853c418825
Justin Gaukler <jpgaukler@gmail.com>
2026-05-20 20:48:44 -0400

    Update outputs.

--- global/outputs.tf
+++ global/outputs.tf
@@ -5,3 +5,8 @@ output "route53_zone_name_servers" {
 output "route53_zone_id" {
   value = aws_route53_zone.app_zone.zone_id
 }
+
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.app_cert.arn
+}
+
