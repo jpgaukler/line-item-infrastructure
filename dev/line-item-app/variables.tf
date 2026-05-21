bd62b56115e784812b0ee462640e62d1ac015036
Justin Gaukler <jpgaukler@gmail.com>
2026-05-20 21:11:34 -0400

    Move line-item-app folder to dev folder.

--- dev/line-item-app/variables.tf
+++ dev/line-item-app/variables.tf
@@ -1,6 +1,6 @@
 locals {
   app_name  = "line-item-app"
-  app_domain = "line-item.app"
+  app_domain = "dev.line-item.app"
   app_logo_uri = "https://line-item.app/images/lineitem_logo_light.svg"
   
   auth0_secrets_key = "auth0_terraform_provider_credentials"
