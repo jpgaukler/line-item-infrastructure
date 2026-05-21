f73015fd61025059334344052c4db90db3f0e2fe
Justin Gaukler <jpgaukler@gmail.com>
2026-05-21 09:24:35 -0400

    Update secret name.

--- dev/line-item-app/variables.tf
+++ dev/line-item-app/variables.tf
@@ -5,7 +5,7 @@ locals {
   app_domain = "${local.environment_stage}.line-item.app"
   app_logo_uri = "https://${local.app_domain}/images/lineitem_logo_light.svg"
   
-  auth0_secrets_key = "auth0_terraform_provider_credentials"
+  auth0_secrets_key = "auth0_terraform_provider_credentials_dev"
   auth0_secrets = jsondecode(data.aws_secretsmanager_secret_version.auth0_keys_latest.secret_string)
   auth0_web_urls = [
     "http://localhost:4200",
