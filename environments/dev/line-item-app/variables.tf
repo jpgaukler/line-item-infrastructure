634a33f209340160f646f39e7827d5dd7fac8cea
Justin Gaukler <jpgaukler@gmail.com>
2026-06-08 07:53:11 -0400

    Clean up secret values for auth0 provider.

--- environments/dev/line-item-app/variables.tf
+++ environments/dev/line-item-app/variables.tf
@@ -6,7 +6,6 @@ locals {
   app_logo_uri = "https://${local.app_domains[0]}/images/lineitem_logo_light.svg"
   
   auth0_secrets_key = "auth0_terraform_provider_credentials_dev"
-  auth0_secrets = jsondecode(data.aws_secretsmanager_secret_version.auth0_keys_latest.secret_string)
   auth0_web_urls = concat(
     ["http://localhost:4200"],
     [for domain in local.app_domains : "https://${domain}"]
